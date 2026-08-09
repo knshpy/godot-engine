ef97c0c0ca92b9e2b60f31b6607d8f16c11c32d9
Kenshi Sabio <kenshisabio09@gmail.com>
2026-08-09 21:24:24 +0800

    my first 2d game

---
+++ nostalgia/scripts/character.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+class_name character
+
+@onready var player: AnimatedSprite2D = $player
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Flipping the player animation
+	if direction == 1:
+		player.flip_h = false
+	elif direction == -1:
+		player.flip_h = true
+		
+	# Walking/Idle animation
+	if direction:
+		player.play("walking")
+	else:
+		player.play("idle")
+	
+	# Jumping animation
+	if not is_on_floor():
+		player.play("jumping")
+		
+	move_and_slide()
